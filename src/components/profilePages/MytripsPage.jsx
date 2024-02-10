a3bb7358c41078b40cd02b20d01d8d2beb913d5c
BhardwajManu <bhabhardwajmamta@gmail.com>
2024-02-10 16:43:56 +0530

    build error

--- src/components/profilePages/MytripsPage.jsx
+++ src/components/profilePages/MytripsPage.jsx
@@ -19,7 +19,7 @@ const MytripsPage = () => {
     if (activeSection === "upcoming") {
       return (
         <div className="mytriplistcontainer-upcoming">
-          <img src={mytriplistimg} alt="mytrip" />
+          {/* <img src={mytriplistimg} alt="mytrip" /> */}
           <div>
             <h3>Looks empty, you've no upcoming bookings.</h3>
             <p>When you book a trip, you will see your itinerary here.</p>
